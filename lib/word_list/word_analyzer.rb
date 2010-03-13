89bc667ca834cd9405665bc08b3d06323600b5ed
blatyo <blatyo@gmail.com>
2010-03-13 07:53:25 -0500

    This solution does a preindexing of what words contain what letters and immediately excludes them from comparison.

--- lib/word_list/word_analyzer.rb
+++ lib/word_list/word_analyzer.rb
@@ -4,33 +4,44 @@ module WordList
       @word_list = word_list
     end
 
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz".split('')
+    
     def analyze
-      reverse_map = @word_list.collect{|word| word.split('').sort.join}
-      word_list = reverse_map.sort
+      word_list = @word_list.collect{|word| word.split('').sort}
       index = build_index word_list
+      all_indices = (0...word_list.length).to_a
 
       curr_word1, curr_word2 = "", ""
-      word_list[0...(word_list.length - 1)].each_with_index do |word1, i|
-        word_list[(index[word1[0]]+1)...word_list.length].each do |word2|
-          unless current_words_have_greater_product?(curr_word1, curr_word2, word1, word2)
-            curr_word1, curr_word2 = word1, word2 unless share_common_letter?(word1, word2)
+      word_list[0...(word_list.length - 1)].each do |word1|
+        possible_pairs(index, word1, all_indices).each do |i|
+          unless current_words_have_greater_product?(curr_word1, curr_word2, word1, word_list[i])
+            curr_word1, curr_word2 = word1, word_list[i]
           end
         end
       end
-      [@word_list[reverse_map.index(curr_word1)], @word_list[reverse_map.index(curr_word2)]] 
+      [@word_list[word_list.index(curr_word1)], @word_list[word_list.index(curr_word2)]] 
+    end
+
+    def possible_pairs(index, word, all_indices)
+      not_pairs = []
+      word.uniq.each do |letter|
+        not_pairs = not_pairs + index[letter]
+      end
+      all_indices - not_pairs
     end
 
     def build_index(word_list)
       index = {}
-      word_list.each_with_index do |word, ind|
-        index[word[0]] = ind 
+      ALPHABET.each{|letter| index[letter] = []}
+      word_list.each_with_index do |word, i|
+        word.each{|letter| index[letter] << i} 
       end
       index
     end
 
     def share_common_letter?(word1, word2)
-      w1_iter = word1.each_char
-      w2_iter = word2.each_char
+      w1_iter = word1.each
+      w2_iter = word2.each
 
       c1 = w1_iter.next
       c2 = w2_iter.next
