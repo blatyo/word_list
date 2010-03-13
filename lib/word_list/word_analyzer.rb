f0ae0bf78f4d4ba1468bc5b66bbe39103564ed9d
blatyo <blatyo@gmail.com>
2010-03-13 12:21:20 -0500

    Small enhancement to exclude more words. Brings runtime on small list to about 0.45 seconds.

--- lib/word_list/word_analyzer.rb
+++ lib/word_list/word_analyzer.rb
@@ -3,8 +3,6 @@ module WordList
     def initialize(word_list)
       @word_list = word_list
     end
-
-    ALPHABET = "abcdefghijklmnopqrstuvwxyz".split('')
     
     def analyze
       word_list = @word_list.sort do |w1, w2|
@@ -14,15 +12,19 @@ module WordList
       longest_length = word_list[0][2]
 
       curr_word1, curr_word2, curr_prod = [], [], 0
-      index = {}
-      ALPHABET.each{|letter| index[letter] = []}
+      index = {'a' => [],'b' => [],'c' => [],'d' => [],'e' => [],'f' => [],'g' => [],'h' => [],'i' => [],'j' => [],
+               'k' => [],'l' => [],'m' => [],'n' => [],'o' => [],'p' => [],'q' => [],'r' => [],'s' => [],'t' => [],
+               'u' => [],'v' => [],'w' => [],'x' => [],'y' => [],'z' => [],'skip' => []}
       word_list.each_with_index do |word, i|
-        next if (beat = curr_prod / word[2]) > longest_length
+        if (beat = curr_prod / word[2]) > longest_length
+          index['skip'] << i
+          next
+        end
         (word[1] = word[0].split('').uniq).each{|letter| index[letter] << i}
         j = best_pair(index, word[1], all_indices, i)
         next unless j
-        next if (word2 = word_list[j])[2] < beat
-        curr_word1, curr_word2, curr_prod = word, word2, word[2] * word2[2]
+        next if word_list[j][2] < beat
+        curr_word1, curr_word2, curr_prod = word, word_list[j], word[2] * word_list[j][2]
       end
       [curr_word1[0], curr_word2[0]]
     end
@@ -32,36 +34,7 @@ module WordList
       word.uniq.each do |letter|
         not_pairs = not_pairs + index[letter]
       end
-      (all_indices[0...current] - not_pairs).first
-    end
-
-    def build_index(word_list)
-
-    end
-
-    def share_common_letter?(word1, word2)
-      w1_iter = word1.each
-      w2_iter = word2.each
-
-      c1 = w1_iter.next
-      c2 = w2_iter.next
-      while(true)
-        begin
-          if c1 == c2
-            return true
-          elsif c1 < c2
-            c1 = w1_iter.next
-          else
-            c2 = w2_iter.next
-          end
-        rescue
-          return false
-        end
-      end
-    end
-
-    def current_words_have_lesser_product?(curr_product, word1, word2)
-      curr_product < word1.length * word2.length
+      (all_indices[0...current] - (not_pairs + index['skip'])).first
     end
   end
 end
