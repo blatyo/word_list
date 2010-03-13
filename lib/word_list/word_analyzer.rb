a88e4aa8da485e1bef8e626e72fa56de07164d5d
blatyo <blatyo@gmail.com>
2010-03-12 19:22:58 -0500

    Added index check so that large portions of strings can be skipped in comparison and swapped product comparison vs same letter comparison per ray's suggestion

--- lib/word_list/word_analyzer.rb
+++ lib/word_list/word_analyzer.rb
@@ -5,23 +5,32 @@ module WordList
     end
 
     def analyze
-      word_list = @word_list.collect{|word| word.split('').sort}
+      reverse_map = @word_list.collect{|word| word.split('').sort.join}
+      word_list = reverse_map.sort
+      index = build_index word_list
 
-      curr_word1, curr_word2 = nil, nil
-      word_list.each_with_index do |word1, i|
-        word_list[i...word_list.length].each_with_index do |word2, j|
-          if word1 != word2 && !share_common_letter?(word1, word2)
-            curr_word1, curr_word2 = word1, word2 if (!curr_word1 && !curr_word2) ||
-              !current_words_have_greater_product?(curr_word1, curr_word2, word1, word2)
+      curr_word1, curr_word2 = "", ""
+      word_list[0...(word_list.length - 1)].each_with_index do |word1, i|
+        word_list[(index[word1[0]]+1)...word_list.length].each do |word2|
+          unless current_words_have_greater_product?(curr_word1, curr_word2, word1, word2)
+            curr_word1, curr_word2 = word1, word2 unless share_common_letter?(word1, word2)
           end
-        end if i < word_list.length - 1
+        end
       end
-      [@word_list[word_list.index(curr_word1)], @word_list[word_list.index(curr_word2)]] 
+      [@word_list[reverse_map.index(curr_word1)], @word_list[reverse_map.index(curr_word2)]] 
+    end
+
+    def build_index(word_list)
+      index = {}
+      word_list.each_with_index do |word, ind|
+        index[word[0]] = ind 
+      end
+      index
     end
 
     def share_common_letter?(word1, word2)
-      w1_iter = word1.each
-      w2_iter = word2.each
+      w1_iter = word1.each_char
+      w2_iter = word2.each_char
 
       c1 = w1_iter.next
       c2 = w2_iter.next
